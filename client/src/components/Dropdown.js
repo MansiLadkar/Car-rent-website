7fd1f52970cab36b557cc9d83f4492f0af256383
Mansi Ladkar <ladkarmansi@gmail.com>
2022-05-07 20:48:56 +0530

    first commit

---
+++ client/src/components/Dropdown.js
@@ -0,0 +1,86 @@
+export const bookedTimeSlots = [
+  {
+    text: "Monday",
+    value: "Mon",
+    key: "Mon",
+  },
+  {
+    text: "Tuesday",
+    value: "Tue",
+    key: "Tue",
+  },
+  {
+    text: "Wednesday",
+    value: "Wed",
+    key: "Wed",
+  },
+  {
+    text: "Thursday",
+    value: "Thu",
+    key: "Thu",
+  },
+  {
+    text: "Friday",
+    value: "Fri",
+    key: "Fri",
+  },
+  {
+    text: "Saturday",
+    value: "Sat",
+    key: "Sat",
+  },
+  {
+    text: "Sunday",
+    value: "Sun",
+    key: "Sun",
+  },
+];
+
+export const transmission = [
+  {
+    text: "Manual",
+    value: "Manual",
+    key: "Manual",
+  },
+  {
+    text: "Automatic",
+    value: "Automatic",
+    key: "Automatic",
+  },
+];
+
+export const carType = [
+  {
+    text: "Hatchback",
+    value: "Hatchback",
+    key: "Hatchback",
+  },
+  {
+    text: "Sedan",
+    value: "Sedan",
+    key: "Sedan",
+  },
+  {
+    text: "SUV",
+    value: "SUV",
+    key: "SUV",
+  },
+  {
+    text: "Mini SUV",
+    value: "Mini SUV",
+    key: "Mini SUV",
+  },
+];
+
+export const fuelType = [
+  {
+    text: "Petrol",
+    value: "Petrol",
+    key: "Petrol",
+  },
+  {
+    text: "Diesel",
+    value: "Diesel",
+    key: "Diesel",
+  },
+];
